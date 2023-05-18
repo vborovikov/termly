1f4467c511ace24917bd2030a4a96ea4a5dd7c4a
Vladislav Borovikov <vborovikov@live.ru>
2023-05-18 10:18:04 +0500

    added documentation comments

--- src/Termly/Colorizer.InterpolatedStringColorHandler.cs
+++ src/Termly/Colorizer.InterpolatedStringColorHandler.cs
@@ -6,29 +6,55 @@ using System.Runtime.CompilerServices;
 
 public static partial class Colorizer
 {
+    /// <summary>
+    /// Writes an interpolated string with colorization to a <see cref="TextWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to write colorized output to.</param>
+    /// <param name="colorHandler">The <see cref="InterpolatedStringColorHandler"/> to use for colorizing output.</param>
     public static void WriteInColor(this TextWriter writer,
         [InterpolatedStringHandlerArgument(nameof(writer))] ref InterpolatedStringColorHandler colorHandler)
     {
         writer.Write(colorHandler.ToStringAndClear());
     }
 
+    /// <summary>
+    /// Writes an interpolated string with colorization to a <see cref="TextWriter"/> followed by a line terminator.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to write colorized output to.</param>
+    /// <param name="colorHandler">The <see cref="InterpolatedStringColorHandler"/> to use for colorizing output.</param>
     public static void WriteLineInColor(this TextWriter writer,
         [InterpolatedStringHandlerArgument(nameof(writer))] ref InterpolatedStringColorHandler colorHandler)
     {
         writer.WriteLine(colorHandler.ToStringAndClear());
     }
 
+    /// <summary>
+    /// Provides an interpolated string handler for colorizing output to a <see cref="TextWriter"/>.
+    /// </summary>
     [InterpolatedStringHandler]
     public ref struct InterpolatedStringColorHandler
     {
         private readonly bool isEnabled;
         private DefaultInterpolatedStringHandler handler;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InterpolatedStringColorHandler"/> struct with the given parameters.
+        /// </summary>
+        /// <param name="literalLength">The length of the literal portion of the interpolated string.</param>
+        /// <param name="formattedCount">The number of formatted expressions in the interpolated string.</param>
+        /// <param name="writer">The <see cref="TextWriter"/> to write colorized output to.</param>
         public InterpolatedStringColorHandler(int literalLength, int formattedCount, TextWriter writer)
             : this(literalLength, formattedCount, null, writer)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InterpolatedStringColorHandler"/> struct with the given parameters.
+        /// </summary>
+        /// <param name="literalLength">The length of the literal portion of the interpolated string.</param>
+        /// <param name="formattedCount">The number of formatted expressions in the interpolated string.</param>
+        /// <param name="provider">An <see cref="IFormatProvider"/> to use for formatting.</param>
+        /// <param name="writer">The <see cref="TextWriter"/> to write colorized output to.</param>
         public InterpolatedStringColorHandler(int literalLength, int formattedCount, IFormatProvider? provider, TextWriter writer)
         {
             this.isEnabled = IsEnabledFor(writer);
@@ -37,20 +63,32 @@ public static partial class Colorizer
             this.handler = new(literalLength, formattedCount, provider);
         }
 
+        /// <inheritdoc/>
         public override string ToString() => this.handler.ToString();
 
+        /// <summary>Gets the built <see cref="string"/> and clears the handler.</summary>
+        /// <returns>The built string.</returns>
         internal ReadOnlySpan<char> ToStringAndClear() => this.handler.ToStringAndClear();
 
+        /// <summary>Writes the specified string to the handler.</summary>
+        /// <param name="value">The string to write.</param>
         public void AppendLiteral(string value)
         {
             this.handler.AppendLiteral(value);
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <typeparam name="T">The type of the value to write.</typeparam>
         public void AppendFormatted<T>(T value)
         {
             this.handler.AppendFormatted(value);
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="format">The format string.</param>
+        /// <typeparam name="T">The type of the value to write.</typeparam>
         public void AppendFormatted<T>(T value, string? format)
         {
             if (HasColor(format, out var colorCode))
@@ -65,11 +103,22 @@ public static partial class Colorizer
             }
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="alignment">Minimum number of characters that should be written for this value.
+        /// If the value is negative, it indicates left-aligned and the required minimum is the absolute value.</param>
+        /// <typeparam name="T">The type of the value to write.</typeparam>
         public void AppendFormatted<T>(T value, int alignment)
         {
             this.handler.AppendFormatted(value, alignment);
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="alignment">Minimum number of characters that should be written for this value.
+        /// If the value is negative, it indicates left-aligned and the required minimum is the absolute value.</param>
+        /// <typeparam name="T">The type of the value to write.</typeparam>
         public void AppendFormatted<T>(T value, int alignment, string? format)
         {
             if (HasColor(format, out var colorCode))
@@ -84,11 +133,17 @@ public static partial class Colorizer
             }
         }
 
+        /// <summary>Writes the specified character span to the handler.</summary>
+        /// <param name="value">The span to write.</param>
         public void AppendFormatted(ReadOnlySpan<char> value)
         {
             this.handler.AppendFormatted(value);
         }
 
+        /// <summary>Writes the specified string of chars to the handler.</summary>
+        /// <param name="value">The span to write.</param>
+        /// <param name="alignment">Minimum number of characters that should be written for this value.  If the value is negative, it indicates left-aligned and the required minimum is the absolute value.</param>
+        /// <param name="format">The format string.</param>
         public void AppendFormatted(ReadOnlySpan<char> value, int alignment = 0, string? format = null)
         {
             if (HasColor(format, out var colorCode))
@@ -103,11 +158,18 @@ public static partial class Colorizer
             }
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
         public void AppendFormatted(string? value)
         {
             this.handler.AppendFormatted(value);
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="alignment">Minimum number of characters that should be written for this value.
+        /// If the value is negative, it indicates left-aligned and the required minimum is the absolute value.</param>
+        /// <param name="format">The format string.</param>
         public void AppendFormatted(string? value, int alignment = 0, string? format = null)
         {
             if (HasColor(format, out var colorCode))
@@ -122,6 +184,11 @@ public static partial class Colorizer
             }
         }
 
+        /// <summary>Writes the specified value to the handler.</summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="alignment">Minimum number of characters that should be written for this value.
+        /// If the value is negative, it indicates left-aligned and the required minimum is the absolute value.</param>
+        /// <param name="format">The format string.</param>
         public void AppendFormatted(object? value, int alignment = 0, string? format = null)
         {
             if (HasColor(format, out var colorCode))
--- src/Termly/Colorizer.cs
+++ src/Termly/Colorizer.cs
@@ -4,10 +4,19 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
+/// <summary>
+/// Provides utilities for colorizing console output.
+/// </summary>
 public static partial class Colorizer
 {
+    /// <summary>
+    /// The ANSI code used to reset terminal color.
+    /// </summary>
     private const string ResetCode = "\x1b[0m";
 
+    /// <summary>
+    /// The ANSI codes used to set foreground color for each <see cref="ConsoleColor"/>.
+    /// </summary>
     private static readonly string[] ForegroundCodes =
     {
             "\x1b[30m",     /// <see cref="ConsoleColor.Black"/>
@@ -28,6 +37,9 @@ public static partial class Colorizer
             "\x1b[37;1m",   /// <see cref="ConsoleColor.White"/>
     };
 
+    /// <summary>
+    /// The ANSI codes used to set background color for each <see cref="ConsoleColor"/>.
+    /// </summary>
     private static readonly string[] BackgroundCodes =
     {
             "\x1b[40m",     /// <see cref="ConsoleColor.Black"/>
@@ -48,22 +60,42 @@ public static partial class Colorizer
             "\x1b[47;1m",   /// <see cref="ConsoleColor.White"/>
     };
 
+    // The handle for standard output
     private const int STD_OUTPUT_HANDLE = -11;
+    // The handle for standard error
     private const int STD_ERROR_HANDLE = -12;
+    // The flag used to enable virtual terminal processing on Windows
     private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
 
+    // A native method from kernel32.dll used to get the console mode
     [DllImport("kernel32.dll")]
     private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+    // A native method from kernel32.dll used to set the console mode
     [DllImport("kernel32.dll")]
     private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+    // A native method from kernel32.dll used to get the handle for a standard device
     [DllImport("kernel32.dll", SetLastError = true)]
     private static extern IntPtr GetStdHandle(int nStdHandle);
 
+    /// <summary>
+    /// <c>true</c> if colorization is enabled, <c>false</c> otherwise.
+    /// </summary>
     private static readonly bool isEnabled;
+
+    /// <summary>
+    /// <c>true</c> if colorization is enabled for standard output, <c>false</c> otherwise.
+    /// </summary>
     private static readonly bool isEnabledOut;
+
+    /// <summary>
+    /// <c>true</c> if colorization is enabled for standard error, <c>false</c> otherwise.
+    /// </summary>
     private static readonly bool isEnabledErr;
 
 
+    /// <summary>
+    /// Initializes static members of the <see cref="Colorizer"/> class.
+    /// </summary>
     static Colorizer()
     {
         isEnabled = Environment.GetEnvironmentVariable("NO_COLOR") is null;
@@ -76,6 +108,16 @@ public static partial class Colorizer
         }
     }
 
+    /// <summary>
+    /// Returns a string representation of the given object with the specified foreground and background colors applied.
+    /// If the colors can't be applied (e.g. the console doesn't support ANSI colors or the NO_COLOR environment variable is set),
+    /// the method returns the original string.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="obj">The object to generate the colored string for.</param>
+    /// <param name="foreground">The foreground color.</param>
+    /// <param name="background">The background color.</param>
+    /// <returns>A string representation of the given object with the specified foreground and background colors applied.</returns>
     public static string? InColor<T>(this T obj, ConsoleColor foreground, ConsoleColor? background = default)
     {
         var text = obj?.ToString();
@@ -85,9 +127,33 @@ public static partial class Colorizer
         return SpanInColor(text, foreground, background);
     }
 
+    /// <summary>
+    /// Returns a string representation of the given object with the specified foreground and background colors applied
+    /// and formatted according to the specified format string. If the colors can't be applied (e.g. the console
+    /// doesn't support ANSI colors or the NO_COLOR environment variable is set), the method returns the original string.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="obj">The object to generate the colored string for.</param>
+    /// <param name="format">A composite format string.</param>
+    /// <param name="foreground">The foreground color.</param>
+    /// <param name="background">The background color.</param>
+    /// <returns>A string representation of the given object with the specified foreground and background colors applied
+    /// and formatted according to the specified format string.</returns>
     public static string? InColor<T>(this T obj, string? format, ConsoleColor foreground, ConsoleColor? background = default)
         where T : struct, IFormattable => InColor(obj, format, null, foreground, background);
 
+    /// <summary>
+    /// Returns a string representation of the given object with the specified foreground and background colors applied and formatted according to
+    /// the specified format string and format provider. If the colors can't be applied (e.g. the console doesn't support ANSI colors or the NO_COLOR
+    /// environment variable is set), the method returns the original string.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="obj">The object to generate the colored and formatted string for.</param>
+    /// <param name="format">A composite format string.</param>
+    /// <param name="formatProvider">An object that supplies culture-specific formatting information.</param>
+    /// <param name="foreground">The foreground color.</param> /// <param name="background">The background color.</param>
+    /// <returns>A string representation of the given object with the specified foreground and background colors applied and formatted according to
+    /// the specified format string and format provider.</returns>
     public static string? InColor<T>(this T obj, string? format, IFormatProvider? formatProvider, ConsoleColor foreground, ConsoleColor? background = default)
         where T : struct, IFormattable
     {
@@ -98,26 +164,59 @@ public static partial class Colorizer
         return SpanInColor(text, foreground, background);
     }
 
+    /// <summary>
+    /// Writes a string with the specified foreground color to the given <see cref="TextWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to write the colored string to.</param>
+    /// <param name="foreground">The foreground color to use for the string.</param>
+    /// <param name="value">The string to write to the <see cref="TextWriter"/>.</param>
     public static void Write(this TextWriter writer, ConsoleColor foreground, string value)
     {
         writer.Write(value.InColor(foreground));
     }
 
+    /// <summary>
+    /// Writes a string with the specified foreground and background colors to the given <see cref="TextWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to write the colored string to.</param>
+    /// <param name="foreground">The foreground color to use for the string.</param>
+    /// <param name="background">The background color to use for the string.</param>
+    /// <param name="value">The string to write to the <see cref="TextWriter"/>.</param>
     public static void Write(this TextWriter writer, ConsoleColor foreground, ConsoleColor background, string value)
     {
         writer.Write(value.InColor(foreground, background));
     }
 
+    /// <summary>
+    /// Writes a string with the specified foreground color to the given <see cref="TextWriter"/>
+    /// followed by a line terminator.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to write the colored string to.</param>
+    /// <param name="foreground">The foreground color to use for the string.</param>
+    /// <param name="value">The string to write to the <see cref="TextWriter"/>.</param>
     public static void WriteLine(this TextWriter writer, ConsoleColor foreground, string value)
     {
         writer.WriteLine(value.InColor(foreground));
     }
 
+    /// <summary>
+    /// Writes a string with the specified foreground color to the given <see cref="TextWriter"/>
+    /// followed by a line terminator.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to write the colored string to.</param>
+    /// <param name="foreground">The foreground color to use for the string.</param>
+    /// <param name="background">The background color to use for the string.</param>
+    /// <param name="value">The string to write to the <see cref="TextWriter"/>.</param>
     public static void WriteLine(this TextWriter writer, ConsoleColor foreground, ConsoleColor background, string value)
     {
         writer.WriteLine(value.InColor(foreground, background));
     }
 
+    /// <summary>
+    /// Determines if colorization is enabled for the given <see cref="TextWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="TextWriter"/> to check. If <c>null</c>, uses the default behavior.</param>
+    /// <returns><c>true</c> if colorization is enabled for the given <see cref="TextWriter"/>; otherwise, <c>false</c>.</returns>
     private static bool IsEnabledFor(TextWriter? writer = default)
     {
         return writer switch
