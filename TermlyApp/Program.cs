0a2cff4abf82ca66094507af113a4d53028654d2
Vladislav Borovikov <vborovikov@live.ru>
2024-12-09 19:06:56 +0500

    added support for multiple formats in composite format string

--- TermlyApp/Program.cs
+++ TermlyApp/Program.cs
@@ -2,3 +2,5 @@
 
 Console.Out.WriteLineInColor($"{"Hello":blue}, {"World":white|green}!");
 Console.Error.WriteLine(ConsoleColor.Red, $"{"Hello":x}, {"World":x}!");
+Console.Out.WriteLineInColor($"{12345.67:N2:cyan}");
+Console.Out.WriteLineInColor($"{DateTimeOffset.Now:HH:mm:yellow}");
